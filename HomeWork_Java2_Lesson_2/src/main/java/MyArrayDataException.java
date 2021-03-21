81262b68f2e6465559858f7dfbfe901407ff1e6b
Vitaliy <Pilipenko.va@live.ru>
2021-03-21 22:53:27 +0300

    HomeWork_Java2_Lesson_2

--- HomeWork_Java2_Lesson_2/src/main/java/MyArrayDataException.java
+++ HomeWork_Java2_Lesson_2/src/main/java/MyArrayDataException.java
@@ -5,8 +5,6 @@ public class MyArrayDataException extends RuntimeException {
 
     public MyArrayDataException(int i, int j){
         super("Некоректно введены данные внутри массива: строка " + i + " столбец " + j);
-        this.i = i;
-        this.j = j;
 
     }
 }
