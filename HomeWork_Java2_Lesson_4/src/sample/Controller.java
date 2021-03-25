a1fa7cf7900623c294690dbdaac6764daf315eaf
Vitaliy <Pilipenko.va@live.ru>
2021-03-25 21:03:35 +0300

    Добавил не большие дополнения

--- HomeWork_Java2_Lesson_4/src/sample/Controller.java
+++ HomeWork_Java2_Lesson_4/src/sample/Controller.java
@@ -1,12 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    
+
+    @FXML
     public TextField msgField;
 
+    @FXML
     public TextArea mainText;
 
     public void clickBtn() {
