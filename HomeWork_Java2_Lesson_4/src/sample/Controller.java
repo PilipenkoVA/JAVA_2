9ec1fe5c8b73ab9c864b97557891609c9db5120e
Vitaliy <Pilipenko.va@live.ru>
2021-03-25 20:59:38 +0300

    Задание на проверку

---
+++ HomeWork_Java2_Lesson_4/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    
+    public TextField msgField;
+
+    public TextArea mainText;
+
+    public void clickBtn() {
+        if (!msgField.getText().isEmpty()){
+            mainText.appendText(msgField.getText()+"\n");
+            msgField.clear();
+        }
+
+    }
+}
